afe14eeb1bd6c32d1accdfbeca8d018aa2363b06
bintangmcsinaga <bintangsinaga007@gmail.com>
2026-05-07 17:33:15 +0700

    changing backend api

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000';
+// const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000';
 // const API_URL = "https://zmcnx33z-5000.asse.devtunnels.ms"
-
+const API_URL = "/api"
 const api = axios.create({
   baseURL: `${API_URL}/api`,
 });
